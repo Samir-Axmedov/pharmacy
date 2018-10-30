d67ca7b8f9c03c1b00eb823dc83e1fc1edc54c1a
Adam Maziakowski <maziakowskiadam@gmail.com>
2018-10-30 13:57:23 +0100

    godzina 01:54 wreszcie siadło.

---
+++ src/main/java/com/maziakowskiadam/pharmacy/model/Category.java
@@ -0,0 +1,50 @@
+package com.maziakowskiadam.pharmacy.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String code;
+    private String country;
+
+    @OneToMany
+    @JoinColumn( name = "category_id")
+    private List<Medication> meds;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public List<Medication> getMeds() {
+        return meds;
+    }
+
+    public void setMeds(List<Medication> meds) {
+        this.meds = meds;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
